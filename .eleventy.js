a9ff806a91358fca08e329ee450a28ed5928aba4
Jon Callahan <jon_callahan@intuit.com>
2023-03-24 17:49:00 -0500

    feat: adds eleventy and a test page

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const postcssConfig = require("./postcss.config.js");
+const PostCSSPlugin = require("eleventy-plugin-postcss");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./style.css");
+  eleventyConfig.addPassthroughCopy({
+    public: ".",
+    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-600-normal.woff2":
+      "fonts/jetbrains-mono-latin-600-normal.woff2",
+    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-600-normal.woff":
+      "fonts/jetbrains-mono-latin-600-normal.woff",
+  });
+  eleventyConfig.addPlugin(PostCSSPlugin, postcssConfig);
+
+  return {
+    dir: {
+      input: ".",
+      output: "dist",
+    },
+    passthroughFileCopy: true,
+  };
+};
