90f788b68fb957d8315a70711b2e810f44374f47
Jon Callahan <j@joncallahan.com>
2026-01-07 21:12:52 -0600

    feat: new design 👨‍🎨 (#3)
    * feat: redesign
    
    * fix: restore redesign CSS, update project descriptions
    
    * feat: add missing articles to homepage
    
    * chore: update Node version to 24
    
    * fix: design
    
    * fix: remove postcss
    
    * fix: copy styles
    
    * fix: remove old stuff
    
    * fix: it
    
    * fix: width

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,27 @@
-const postcssConfig = require("./postcss.config.js");
-const PostCSSPlugin = require("eleventy-plugin-postcss");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./styles");
   eleventyConfig.addPassthroughCopy({
     public: ".",
-    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-600-normal.woff2":
-      "fonts/jetbrains-mono-latin-600-normal.woff2",
-    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-600-normal.woff":
-      "fonts/jetbrains-mono-latin-600-normal.woff",
-    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-400-normal.woff2":
-      "fonts/jetbrains-mono-latin-400-normal.woff2",
-    "node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-400-normal.woff":
-      "fonts/jetbrains-mono-latin-400-normal.woff",
+    styles: "styles",
+    "node_modules/@fontsource/mona-sans/files": "styles/files",
+    "node_modules/@fontsource/jetbrains-mono/files": "styles/files",
   });
-  eleventyConfig.addPlugin(PostCSSPlugin, postcssConfig);
-  eleventyConfig.addPlugin(syntaxHighlight);
+
+
+  const markdownIt = require("markdown-it");
+  const markdownItShiki = require("markdown-it-shiki").default;
+
+  const md = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+  });
+
+  md.use(markdownItShiki, {
+    theme: "vitesse-black",
+  });
+
+  eleventyConfig.setLibrary("md", md);
 
   return {
     dir: {
